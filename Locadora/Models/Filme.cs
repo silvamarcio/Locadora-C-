7c2eacf7e029818f867b7d56042a0bc85a1f4322
silvamarcio <silva.marcio86@hotmail.com>
2019-04-15 22:03:55 -0300

    Entities created

---
+++ Locadora/Models/Filme.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Filme
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Genre { get; set; }
+        public double Price { get; set; }
+
+
+
+    }
+}
---
+++ Locadora/Models/Locacao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Locadora.Models
+{
+    public class Locacao
+    {
+        public int Id { get; set; }
+        public double RentalPrice { get; set; }
+        public List<Filme> Filmes { get; set; } = new List<Filme>();
+
+        public Locacao()
+        {
+        }
+
+        public Locacao(int id, double rentalPrice)
+        {
+            Id = id;
+            RentalPrice = rentalPrice;
+        }
+
+        public void AddFilme(Filme filme)
+        {
+            Filmes.Add(filme);
+        }
+
+        public void RemoveFilme(Filme filme)
+        {
+            Filmes.Remove(filme);
+        }
+
+        public double SubTotal()
+        {
+            RentalPrice = 0;
+
+            foreach(Filme f in Filmes)
+            {
+                RentalPrice = Filmes.Sum(x => x.Price);
+            }
+
+            return RentalPrice;
+        }
+    }
+}
